fa9f56ae8ccb2ef18785fe2d34455314dd1ba38d
hugopereira-cs <hugoper_cs@yahoo.com>
2025-10-03 18:08:54 -0300

    adiciona versão final do desafio da Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,46 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const maxRecords = 151
+const limit = 5
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    
-                </ol>
 
-                <img src="${
-                    pokemon.photo
-                }" alt="${pokemon.name}">
-            </div>
-        </li>
-   `;
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        
+                    </ol>
+
+                    <img src="${
+                        pokemon.photo
+                    }" alt="${pokemon.name}">
+                </div>
+            </li>
+    `).join('') // mapeia a lista de pokemons para li, e junta todos esses 'li's sem separador
+        pokemonList.innerHTML += newHtml // Atualiza o conteúdo HTML do elemento, preservando o conteúdo existente e adicionando novos pokemons
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList');
+loadPokemonItens(offset, limit)
 
-pokeApi
-    .getPokemons()
-    .then((pokemons = []) => {
-        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join(''); // pega a lista de pokemons, mapeia essa lista para li, e junta todos esses 'li's sem separador
-    })
-    .catch((error) => console.log(error));
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
 
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton) // pega o elemento pai do botão e remove seu filho (o botão)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
